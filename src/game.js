d8804785592e88a623e81450216acf13d7c5cc84
Julián Sanmartino <jjsanmartino03@gmail.com>
2021-07-19 13:42:27 -0300

    fix errors

--- src/game.js
+++ src/game.js
@@ -95,7 +95,7 @@ export default function Game() {
   const movementsList = []; // the array of buttons to change the current step in the game
 
   for (let i = 0; i < history.length; i++) {
-    let [a, b] = history[i].moveLocation;
+    //let [a, b] = history[i].moveLocation;
 
     movementsList.push(
       <button
@@ -138,7 +138,7 @@ export default function Game() {
               <div className="bg-white border-2 rounded border-gray-400 w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-500">
                 <input
                   checked={reverse}
-                  onChange={() => setReverse((reverse) => !reverse)}
+                  onChange={sortMoves}
                   type="checkbox"
                   className="opacity-0 absolute"
                 />
